29965dc633d38cfab3874b1aa35b5e0829777989
Andalexshap <andalexshap@yandex.ru>
2023-02-23 22:54:22 +0300

    Home_work_5

---
+++ Home_work/Home_work_5/Example_1.java
@@ -0,0 +1,48 @@
+package Home_work.Home_work_5;
+
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Stack;
+
+public class Example_1 {
+    public static void main(String[] args) {
+        /*
+ * Написать метод, определяющую правильность расстановки скобок в выражении.
+ * Могут содержаться следующие скобки: ()[]{}<>
+ * () -> true
+ * []{}((<>)) -> true
+ * ) -> false
+ * [) -> false
+ * []{}(<)> -> false
+ */
+        System.out.println(isCorrectParentheses("()")); // -> true
+        System.out.println(isCorrectParentheses("[]{}((<>))")); // -> true
+        System.out.println(isCorrectParentheses(")")); //-> false
+        System.out.println(isCorrectParentheses("[)")); //-> false
+        System.out.println(isCorrectParentheses("[]{}(<)>")); //-> false
+    }
+    private static boolean isCorrectParentheses(String parentheses) {
+        Map<Character, Character> brackets = new HashMap<>();
+    brackets.put(')', '(');
+    brackets.put('}', '{');
+    brackets.put(']', '[');
+    brackets.put('>', '<');
+
+    Deque<Character> stack = new LinkedList<>();
+for (char c : parentheses.toCharArray()) {
+    if (brackets.containsValue(c)) {
+        // одна из открывающих скобок
+        stack.push(c);
+    } else if (brackets.containsKey(c)) {
+        // одна из закрывающих скобок
+        if (stack.isEmpty() || stack.pop() != brackets.get(c)) {
+            return false;
+        }
+    }
+}
+// в конце стек должен быть пустым
+return stack.isEmpty();
+    }
+}
